7d4fb24bd721323366608f66338297ac27486b94
abdelrhmanehab10 <abdelrhmanehab047@gmail.com>
2023-11-02 16:43:56 +0200

    navigaton for slider

--- components/works.tsx
+++ components/works.tsx
@@ -2,9 +2,11 @@ import { FC } from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import "swiper/css";
 import OpinionsCard from "./opinions-card";
+import { Navigation } from "swiper/modules";
 
+import "swiper/css";
+import "swiper/css/navigation";
 interface WorksProps {}
 
 const Works: FC<WorksProps> = ({}) => {
@@ -153,11 +155,14 @@ const Works: FC<WorksProps> = ({}) => {
           Pets - E-commerce
         </div>
       </div>
-      <h2 className="text-main md:text-white font-bold text-center">
-        اراء العملاء
-      </h2>
-      <Swiper spaceBetween={50} slidesPerView={1}>
-        <SwiperSlide className="my-2 w-[80%] mx-auto border border-black rounded-2xl p-5">
+      <h2 className="text-main font-bold text-center">اراء العملاء</h2>
+      <Swiper
+        spaceBetween="50px"
+        navigation={true}
+        modules={[Navigation]}
+        className="md:w-1/2 w-[80%] mx-auto"
+      >
+        <SwiperSlide className="my-2 border border-black rounded-2xl px-10 py-5">
           <OpinionsCard
             clientName="حمزه علاء"
             opinion="يارتني قابلت م/معاذ من زمان ما تتصوروا شلون غيرت موقعنا من ورا ورا وصاير
@@ -165,19 +170,19 @@ const Works: FC<WorksProps> = ({}) => {
     "
           />
         </SwiperSlide>
-        <SwiperSlide className="my-2 w-[80%] mx-auto border border-black rounded-2xl p-5">
+        <SwiperSlide className="my-2 border border-black rounded-2xl px-10 py-5">
           <OpinionsCard
             clientName="سليمان حمزة"
             opinion="شكرا يا بطل أنا وراعي المشروع ما نفهم شي بالسيو ، بس معاذ فهمنا الصح وسوا شغل رهيب صاير يوميا فيه طلبات جديدة."
           />
         </SwiperSlide>
-        <SwiperSlide className="my-2 w-[80%] mx-auto border border-black rounded-2xl p-5">
+        <SwiperSlide className="my-2 border border-black rounded-2xl px-10 py-5">
           <OpinionsCard
             clientName="خديجة سلطان"
             opinion="صدقوني هالولد عنده خبرة كان موقعي مختفي في النت ، بس مع السيو اللي سواه بلشت أشوف تحسن سريع ، ممتنة لك."
           />
         </SwiperSlide>
-        <SwiperSlide className="my-2 w-[80%] mx-auto border border-black rounded-2xl p-5">
+        <SwiperSlide className="my-2 border border-black rounded-2xl px-10 py-5">
           <OpinionsCard
             clientName="محمد صابر"
             opinion="يا جدعان أنا متعامل مع معاذ في سيو ومفيش كلام شغله فصل وجاب نتائج جامدة ، موقعي بقي أول صفحة في جوجل الله يخليك يا وحش."
