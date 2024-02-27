a98836a196c5c56ddd9cf99f5ee31a7eb7f41ebc
Abdelrhman Ehab <abdelrhmanehab047@gmail.com>
2024-02-27 12:05:33 +0200

    fix:content.match expresion

--- postcss.config.js
+++ postcss.config.js
@@ -18,7 +18,7 @@ module.exports = {
           './components/**/*.{js,jsx,ts,tsx}',
         ],
         defaultExtractor: (content) =>
-          content.match(/[\\w-/:]+(?<!:)/g) || [],
+          content.match(/[\\w/-/]+(?<!:)/g) || [],
         safelist: ['html', 'body'],
       },
     ],
